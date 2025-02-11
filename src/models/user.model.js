0d318c3429753e18d27907dda68a0446d15a37ca
sairam269269 <sairam.s269269@gmail.com>
2025-02-11 22:38:23 +0530

    "Implemented password hashing and JWT authentication in User schema"

---
+++ src/models/user.model.js
@@ -0,0 +1,89 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema=new Schema(
+    {
+      username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+      },
+      email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+      },
+      fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+      },
+      avator:{
+        type:string, // cloudnary url
+        required:true
+      },
+      coverImage:{
+        type:string // cloudanry url
+      },
+      watchHistory:[{
+        type:Schema.Types.ObjectId,
+        ref:"video"
+      }],
+
+      password:{
+        type:String,
+        required:[true,"password must required"],
+        unique:true,
+        
+      },
+      refreshToken:{
+        type:string
+      },
+    },
+    {
+        timestamps:true
+      }
+)
+
+userSchema.pre("save",async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password,10)
+  next()
+})
+
+userSchema.methods.generateAccessToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      username:this.username,
+      fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+ export const User=mongoose.model("User",userSchema)
