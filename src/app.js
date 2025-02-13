fe44eb5934d8fd0c529cbd04aa22fa7462a3e6cc
sairam269269 <sairam.s269269@gmail.com>
2025-02-13 14:26:54 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -12,6 +12,11 @@ const app=  express();
    app.use(express.static("public"));
    app.use(cookieParser());
 
+//define router
+import userRouter from "./routes/user.route.js"
+
+  app.use("/api/v1/users",userRouter)
+
 
 
 
